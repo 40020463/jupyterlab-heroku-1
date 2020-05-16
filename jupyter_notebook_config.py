afc37d2940b7b423b3a6157a7b321399d8295ee9
40020463 <65107495+40020463@users.noreply.github.com>
2020-05-16 19:47:50 +0800

    Update jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -4,4 +4,4 @@
 # we have SSL certificates enabled for encryption purposes.
 c.NotebookApp.ip = "*"
 c.NotebookApp.open_browser = False
-c.NotebookApp.password = "zhou6790296"
+c.NotebookApp.password = "sha256:f19629243b3c:371ae8747a706a0f443b66bb9fd4e8d86a3e73caa51f8be88bfaf66697132e0b"
